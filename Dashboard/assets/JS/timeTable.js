e8b4920ef794c6cad0668d604b0eaf2109f6bfd2
Jedidiah Ayobamidele Olugbenga <jedbert@yahoo.com>
2024-05-11 00:51:28 +0100

    Student Dashboard

---
+++ Dashboard/assets/JS/timeTable.js
@@ -0,0 +1,104 @@
+const Sunday =[
+    {   
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        Module: 'No class Available',
+        mode: ''
+    }
+]
+const Monday =[
+    {   
+        time: '09-10 AM',
+        Module: 'DBMS130',
+        mode: 'Online'
+    },
+    {   
+        time: '10-11 AM',
+        Module: 'DBMS130',
+        mode: 'Online'
+    },
+    {   
+        time: '12-01 PM',
+        Module: 'DBMS130',
+        mode: 'Online'
+    }
+]
+const Tuesday =[
+    {   
+        time: '09-10 AM',
+        Module: 'DBMS130',
+        mode: 'Online'
+    },
+    {   
+        time: '10-11 AM',
+        Module: 'DBMS130',
+        mode: 'Online'
+    },
+    {   
+        time: '12-01 PM',
+        Module: 'DBMS130',
+        mode: 'Online'
+    }
+]
+
+const Wednesday =[
+    {     
+        time: '09-10 AM',
+        Module: 'DBMS130',
+        mode: 'Online'
+    },
+    {   
+        time: '10-11 AM',
+        Module: 'DBMS130',
+        mode: 'Online'
+    },
+    {   
+        time: '12-02 PM',
+        Module: 'DBMS130',
+        mode: 'Physical Class'
+    }
+]
+
+const Thursday =[
+   {   
+        time: '09-10 AM',
+        Module: 'DBMS130',
+        mode: 'Online'
+    },
+    {   
+        time: '10-11 AM',
+        Module: 'DBMS130',
+        mode: 'Online'
+    },
+    {   
+        time: '12-01 PM',
+        Module: 'DBMS130',
+        mode: 'Online'
+    }
+]
+
+const Friday =[
+   {   
+        time: '09-10 AM',
+        Module: 'DBMS130',
+        mode: 'Online'
+    },
+    {   
+        time: '10-11 AM',
+        Module: 'DBMS130',
+        mode: 'Online'
+    },
+    {   
+        time: '12-01 PM',
+        Module: 'DBMS130',
+        mode: 'Online'
+    }
+]
+
+const Saturday =[
+    {   
+        time: 'Saturday',
+        Module: 'No class Available',
+        mode: ''
+    }
+]
